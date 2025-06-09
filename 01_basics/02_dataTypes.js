8e1523ab94dc5504265af95e5d7a23d5046828bb
ankurr034 <ankurrrastogi034@gmail.com>
2025-06-09 19:45:00 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+let name = " Ankur"
+let age = 20
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+//  bigint 
+//  string => ""
+//  boolean => true/false
+// null => satndalone value
+//  undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
